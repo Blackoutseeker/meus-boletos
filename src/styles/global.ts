2ba6dbdf34016201d61b7f29d2a62d4b582b025c
Blackoutseeker <56811005+Blackoutseeker@users.noreply.github.com>
2021-03-16 14:11:11 -0300

    modified:   src/styles/global.ts

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,10 +5,10 @@ export default createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    font-family: Roboto;
     transition: 0.2s;
   }
   body {
     background-color: ${({ theme }) => theme.colors.background};
-    font-family: Roboto;
   }
 `
