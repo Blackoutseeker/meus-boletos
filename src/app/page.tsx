f74ed78b53de8317a4721ed712e2fe9515c9d3c5
Blackoutseeker <felipeoriginal.2019@gmail.com>
2025-06-05 04:40:27 -0300

    feat(app): create page.tsx file

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center"></main>
+  )
+}
+
+export default Home
