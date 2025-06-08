f67335eaae87ca49f887be2ce4aea4b777d6d611
Blackoutseeker <felipeoriginal.2019@gmail.com>
2025-06-08 07:10:17 -0300

    refactor(app): add Header component to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 import type { NextPage } from 'next'
+import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center"></main>
+    <main className="flex min-h-screen flex-col items-center">
+      <Header />
+    </main>
   )
 }
 
