31c6fe9df9fbd5955eafd5eac3aa7bd70de5e032
Blackoutseeker <felipeoriginal.2019@gmail.com>
2025-06-09 05:03:44 -0300

    refactor(components): remove CSS conflict file from Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,10 @@
 import type { FC } from 'react'
 import { FaUser } from 'react-icons/fa6'
-import './styles.css'
 
 const Header: FC = () => {
   return (
-    <header className="header">
-      <button className="login-button">
+    <header className="bg-dark-900 sticky top-0 flex w-full items-center justify-center py-10 md:justify-end md:px-10">
+      <button className="bg-dark-800 hover:bg-dark-300 flex cursor-pointer space-x-5 rounded-full px-5 py-3 duration-150">
         <span className="font-bold">Entrar</span>
         <FaUser color="white" size={16} />
       </button>
