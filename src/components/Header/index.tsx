c48db7aebfee17e212609926e9cc09c37ff1be50
Blackoutseeker <felipeoriginal.2019@gmail.com>
2025-06-10 07:05:21 -0300

    fix(components): center elements in login button from Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,7 +4,7 @@ import { FaUser } from 'react-icons/fa6'
 const Header: FC = () => {
   return (
     <header className="bg-dark-900 sticky top-0 flex w-full items-center justify-center py-10 md:justify-end md:px-10">
-      <button className="bg-dark-800 hover:bg-dark-300 flex cursor-pointer space-x-5 rounded-full px-5 py-3 duration-150">
+      <button className="bg-dark-800 hover:bg-dark-300 flex cursor-pointer items-center space-x-5 rounded-full px-5 py-3 duration-150">
         <span className="font-bold">Entrar</span>
         <FaUser color="white" size={16} />
       </button>
