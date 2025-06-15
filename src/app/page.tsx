6f8c1d19695d87ebe5cb46a9515724b9959c9f9b
Blackoutseeker <felipeoriginal.2019@gmail.com>
2025-06-15 17:02:59 -0300

    feat(app): add Footer component to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import type { NextPage } from 'next'
 import StoreProvider from './StoreProvider'
 import Header from '../components/Header'
+import Footer from '../components/Footer'
 
 const Home: NextPage = () => {
   return (
     <main className="flex min-h-screen flex-col items-center">
       <StoreProvider>
         <Header />
+        <Footer />
       </StoreProvider>
     </main>
   )
