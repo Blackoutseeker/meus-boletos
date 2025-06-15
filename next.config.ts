55f48ebfdb6b3f97283cd4a30faf98c33bb11773
Blackoutseeker <felipeoriginal.2019@gmail.com>
2025-06-15 16:16:10 -0300

    chore(next): add AUTH_PASSWORD environment variable to Next.js configuration file

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,9 @@ const nextConfig: NextConfig = {
     PROJECT_ID: process.env.PROJECT_ID,
     STORAGE_BUCKET: process.env.STORAGE_BUCKET,
     MESSAGING_SENDER_ID: process.env.MESSAGING_SENDER_ID,
-    APP_ID: process.env.APP_ID
+    APP_ID: process.env.APP_ID,
+
+    AUTH_PASSWORD: process.env.AUTH_PASSWORD
   }
 }
 
