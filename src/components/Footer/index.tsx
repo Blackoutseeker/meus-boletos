605a5160b34ef846f2ddccc9e3db5260852e31ab
Blackoutseeker <felipeoriginal.2019@gmail.com>
2025-06-15 17:02:15 -0300

    refactor(components): use Login reducer to show the Footer component

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,14 +1,26 @@
+'use client'
+
 import type { FC } from 'react'
+import { useAppSelector } from '@/src/hooks/redux'
 import { IoAddSharp } from 'react-icons/io5'
 
 const Footer: FC = () => {
-  return (
-    <footer className="bg-dark-900 sticky bottom-0 flex w-full items-center justify-end p-10">
-      <button className="cursor-pointer rounded-full bg-green-500 p-5 duration-150 hover:bg-green-400">
-        <IoAddSharp color="black" size={20} />
-      </button>
-    </footer>
-  )
+  const { isLoggedIn } = useAppSelector(state => state.loginReducer)
+
+  if (isLoggedIn) {
+    return (
+      <>
+        <div className="flex flex-grow" />
+        <footer className="bg-dark-900 sticky bottom-0 flex w-full items-center justify-end p-10">
+          <button className="cursor-pointer rounded-full bg-green-500 p-5 duration-150 hover:bg-green-400">
+            <IoAddSharp color="black" size={20} />
+          </button>
+        </footer>
+      </>
+    )
+  }
+
+  return <div className="py-5" />
 }
 
 export default Footer
