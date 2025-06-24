29465d311ec94ed5812dab07d0ffa0bfec9cc73d
Blackoutseeker <felipeoriginal.2019@gmail.com>
2025-06-24 04:56:35 -0300

    feat(storage): create uploadDocument method

---
+++ src/storage/document.ts
@@ -0,0 +1,18 @@
+import { storage } from '@/src/utils/firebase'
+import type { StorageReference } from 'firebase/storage'
+import { ref, uploadBytes, getDownloadURL } from 'firebase/storage'
+
+export const uploadDocument = async (
+  id: string,
+  document: File
+): Promise<string> => {
+  const documentReference: StorageReference = ref(
+    storage,
+    `documents/${id}.pdf`
+  )
+  await uploadBytes(documentReference, document, {
+    contentType: 'application/pdf'
+  })
+  const downloadUrl: string = await getDownloadURL(documentReference)
+  return downloadUrl
+}
