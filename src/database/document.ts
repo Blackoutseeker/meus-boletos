3f9be09ceaa628e99c776af9c588d549ac9c0a92
Blackoutseeker <felipeoriginal.2019@gmail.com>
2025-06-24 04:57:17 -0300

    feat(database): create pushDocument method

--- src/database/document.ts
+++ src/database/document.ts
@@ -1,6 +1,6 @@
 import type { Document } from '@/src/models/document'
 import { database } from '@/src/utils/firebase'
-import { ref, get } from 'firebase/database'
+import { ref, get, push } from 'firebase/database'
 
 export const getDocuments = async (): Promise<Document[]> => {
   const documents: Document[] = []
@@ -18,4 +18,25 @@ export const getDocuments = async (): Promise<Document[]> => {
   }
 
   return documents
+}
+
+export const pushDocument = async (
+  title: string,
+  password: string,
+  downloadUrl: string,
+  expirationDate?: string
+): Promise<string> => {
+  const documentsRef = ref(database, 'documents')
+  const document = {
+    title,
+    password,
+    downloadUrl,
+    expirationDate
+  }
+
+  if (!expirationDate) {
+    delete document.expirationDate
+  }
+
+  return push(documentsRef, document).key
 }
