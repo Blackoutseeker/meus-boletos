494796ffa4b44c85047f683b9612debc85d1ba9e
Blackoutseeker <felipeoriginal.2019@gmail.com>
2025-06-24 05:20:45 -0300

    refactor(components): use Document type as property in DocumentCard component

--- src/components/Document/index.tsx
+++ src/components/Document/index.tsx
@@ -1,17 +1,26 @@
 import type { FC } from 'react'
+import type { Document } from '@/src/models/document'
 import { HiDownload } from 'react-icons/hi'
 import { FaTrash } from 'react-icons/fa'
 
-const Document: FC = () => {
+interface DocumentCardProps {
+  document: Document
+}
+
+const DocumentCard: FC<DocumentCardProps> = ({ document }) => {
   const isAuthenticated: boolean = true
 
   return (
     <li className="border-dark-100 flex min-h-[240px] w-full max-w-[360px] min-w-[240px] flex-col space-y-5 rounded-lg border p-5">
-      <h2 className="text-xl">Boleto</h2>
+      <h2 className="text-xl">{document.title}</h2>
       <div className="flex-grow" />
-      <p className="text-sm text-white/70">Vencimento: 09/06/2025</p>
+      {document.expirationDate && (
+        <p className="text-sm text-white/70">
+          Vencimento: {document.expirationDate}
+        </p>
+      )}
       <div className="flex space-x-2">
-        <button className="bg-dark-500 hover:bg-dark-100 flex w-full cursor-pointer items-center justify-center space-x-5 rounded-md p-3 duration-150">
+        <button className="bg-dark-500 hover:bg-dark-100 flex w-full cursor-pointer items-center justify-center space-x-3 rounded-md p-3 duration-150">
           <span className="uppercase">Download</span>
           <HiDownload size={24} />
         </button>
@@ -25,4 +34,4 @@ const Document: FC = () => {
   )
 }
 
-export default Document
+export default DocumentCard
