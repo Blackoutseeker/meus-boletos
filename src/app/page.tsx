887c7f3b31ab5c800965ded2dc68716693bece49
Blackoutseeker <felipeoriginal.2019@gmail.com>
2025-06-24 05:59:20 -0300

    feat(app): add DocumentList component to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,26 @@
 import type { NextPage } from 'next'
+import type { Document } from '@/src/models/document'
+import { getDocuments } from '@/src/database/document'
 import StoreProvider from './StoreProvider'
 import Header from '../components/Header'
+import DocumentList from '../components/DocumentList'
 import Footer from '../components/Footer'
 
-const Home: NextPage = () => {
+const Home: NextPage = async () => {
+  const initialDocuments: Document[] = await getDocuments()
+
   return (
     <main className="flex min-h-screen flex-col items-center">
       <StoreProvider>
         <Header />
+        <div className="flex w-full items-center justify-center px-5">
+          {initialDocuments.length > 0 && (
+            <DocumentList initialDocuments={initialDocuments} />
+          )}
+          {initialDocuments.length === 0 && (
+            <h2 className="text-xl font-bold">Nenhum documento encontrado.</h2>
+          )}
+        </div>
         <Footer />
       </StoreProvider>
     </main>
