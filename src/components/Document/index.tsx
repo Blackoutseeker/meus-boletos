42ec4212ee7ae0f7c487025f7d49367caf63a36f
Blackoutseeker <felipeoriginal.2019@gmail.com>
2025-06-26 02:24:54 -0300

    refactor(components): add InputContainer to DocumentCard component

--- src/components/Document/index.tsx
+++ src/components/Document/index.tsx
@@ -1,15 +1,12 @@
 import type { FC } from 'react'
 import type { Document } from '@/src/models/document'
-import { HiDownload } from 'react-icons/hi'
-import { FaTrash } from 'react-icons/fa'
+import InputContainer from './InputContainer'
 
 interface DocumentCardProps {
   document: Document
 }
 
 const DocumentCard: FC<DocumentCardProps> = ({ document }) => {
-  const isAuthenticated: boolean = true
-
   return (
     <li className="border-dark-100 flex min-h-[240px] w-full max-w-[360px] min-w-[240px] flex-col space-y-5 rounded-lg border p-5">
       <h2 className="text-xl">{document.title}</h2>
@@ -20,15 +17,7 @@ const DocumentCard: FC<DocumentCardProps> = ({ document }) => {
         </p>
       )}
       <div className="flex space-x-2">
-        <button className="bg-dark-500 hover:bg-dark-100 flex w-full cursor-pointer items-center justify-center space-x-3 rounded-md p-3 duration-150">
-          <span className="uppercase">Download</span>
-          <HiDownload size={24} />
-        </button>
-        {isAuthenticated && (
-          <button className="flex w-fit cursor-pointer items-center justify-center space-x-5 rounded-md bg-red-800 px-4 py-3 duration-150 hover:bg-red-700">
-            <FaTrash size={16} />
-          </button>
-        )}
+        <InputContainer document={document} />
       </div>
     </li>
   )
