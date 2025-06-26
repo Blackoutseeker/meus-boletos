bc9f21c7b90a0809af1aa8cc13e0cf756df688e0
Blackoutseeker <felipeoriginal.2019@gmail.com>
2025-06-26 03:55:22 -0300

    feat(utils): create a regex to get the document's ID from download URL

--- src/utils/regex.ts
+++ src/utils/regex.ts
@@ -1,3 +1,5 @@
 export const expirationDateRegex: RegExp = /^\d{2}\/\d{2}\/\d{4}$/g
 
 export const documentBadIdRegex: RegExp = /[ .#$\\[\]/]/g
+
+export const downloadIdRegex: RegExp = /documents%2F(.+?)\.pdf/
